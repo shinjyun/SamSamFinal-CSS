e58a791fae35b5111b28f704f23979994fa69a6f
shinjyun <shinproject21@gmail.com>
2024-07-13 00:36:52 +0900

    fix: [MemberHeader.js] 로그인, 회원가입, 판매하기 버튼 배치

--- front/src/member/page/MemberHeader.js
+++ front/src/member/page/MemberHeader.js
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import '../../App.css';
 
 import LogoutButton from "../component/button/LogoutButton";
 import ToJoinPageButton from "../component/button/ToJoinPageButton";
@@ -14,79 +15,54 @@ import Col from "react-bootstrap/esm/Col";
 
 import { Link } from "react-router-dom";
 
-function MemberHeader({handleStorageChange, memberId}){
 
-    return(
+function MemberHeader({ handleStorageChange, memberId }) {
 
-        <div>
+	return (
 
-            <Navbar className="bg-body-tertiary">
-                <Container className="d-flex justify-content-between align-items-center">
-                    <Navbar.Brand href="/" className="d-flex align-items-center">
-                    <Col xs="auto">
-                        <img
-                        alt=""
-                        src={process.env.PUBLIC_URL + "/samsamlogo.png"}
-                        width="80"
-                        height="80"
-                        className="d-inline-block align-top"
-                        />
-                    </Col>
-                    <Col>
-                        <h1 className="mb-0 ms-2" style={{fontFamily: 'Jua'}}>SAMSAM</h1>
-                    </Col>
-                        {memberId === "admin" && <Link to={"/admin"} 
-                        style={{margin: '20px', padding: '5px 15px', borderRadius: '30px', textDecoration: 'None', backgroundColor: '#21E6C1', color: 'white', fontWeight: 'bold' }}>관리자 페이지</Link>}
-                    </Navbar.Brand>
-        
+		<div>
 
-                <div className="d-flex align-items-center">
-                    <SellProductButton memberId={memberId} handleStorageChange={handleStorageChange}/>
-
-                    {/* 로그인 관련 컴포넌트 */}
-                    <div style={{display: 'inline'}}>
-                        {memberId === null ? (
-                            <>
-                                <LoginButton handleStorageChange={handleStorageChange}/>
-                                <ToJoinPageButton />
-                            </>
-                                ) : (
-                            <>
-                            <LogoutButton handleStorageChange={handleStorageChange} />
-                            <MyPageButton member_id={memberId} />
-                            </>
-                        )}
-
-                    </div>
-
-                </div>
-
-            </Container>
-        </Navbar>
+			<Navbar className="bg-body-tertiary">
+				<Container className="d-flex justify-content-between align-items-center">
+					<Navbar.Brand href="/" className="d-flex align-items-center">
+						<Col xs="auto">
+							<img
+								alt=""
+								src={process.env.PUBLIC_URL + "/samsamlogo.png"}
+								width="80"
+								height="80"
+								className="d-inline-block align-top"
+							/>
+						</Col>
+						<Col>
+							<h1 className="mb-0 ms-2" style={{ fontFamily: 'Jua' }}>SAMSAM</h1>
+						</Col>
+						{memberId === "admin" && <Link to={"/admin"} className="memberheader-admin-link">관리자 페이지</Link>}
+					</Navbar.Brand>
 
 
-            {/* <SellProductButton memberId={memberId} handleStorageChange={handleStorageChange}/> */}
 
-            {/* 로그인 관련 컴포넌트 */}
-            {/* <div style={{display: 'inline'}}>
-                {memberId === null ? (
-                    <>
-                        <LoginButton handleStorageChange={handleStorageChange}/>
-                        <ToJoinPageButton />
-                    </>
-                        ) : (
-                    <>
-                    <LogoutButton handleStorageChange={handleStorageChange} />
-                    <MyPageButton member_id={memberId} />
-                    </>
-                )}
- */}
-                {/* {memberId === "admin" && <Link to={"/admin"}>admin page</Link>} */}
-            {/* </div> */}
-
-        </div>
-
-    )
+					{/* 로그인 관련 컴포넌트 */}
+					<div className="auth-buttons-container" style={{ display: 'flex', alignItems: 'center' }}>
+						{memberId === null ? (
+							<div className="auth-buttons">
+								<LoginButton handleStorageChange={handleStorageChange} />
+								<ToJoinPageButton />
+							</div>
+						) : (
+							<div className="auth-buttons">
+								<LogoutButton handleStorageChange={handleStorageChange} />
+								<MyPageButton member_id={memberId} />
+							</div>
+						)}
+						<div className="sell-button-container">
+							<SellProductButton memberId={memberId} handleStorageChange={handleStorageChange} />
+						</div>
+					</div>
+				</Container>
+			</Navbar>
+		</div>
+	);
 }
 
 export default MemberHeader;
--- front/src/webpage/cs/CSSelect.js
+++ front/src/webpage/cs/CSSelect.js
@@ -7,6 +7,8 @@ import '../../../src/App.css';
 import ToMainPageButton from '../../member/component/button/ToMainPageButton';
 import MemberHeader from '../../member/page/MemberHeader';
 
+import { Pagination, Row, Form, Button, Dropdown, Table, Container } from 'react-bootstrap';
+
 const CSSelect = ({handleStorageChange, memberId}) => {
     const [inquiries, setInquiries] = useState([]);
     const [searchContent, setSearchContent] = useState('title');
@@ -21,12 +23,14 @@ const CSSelect = ({handleStorageChange, memberId}) => {
 
 
 
-    const handleSearchContentChange = (e) => {
-        setSearchContent(e.target.value);
+    const handleSearchContentChange = (content) => {
+        setSearchContent(content);
+        // setSearchContent(value);
     };
 
     const handleSearchKeywordChange = (e) => {
         setSearchKeyword(e.target.value);
+        // setSearchKeyword(value);
     };
 
     const handleSearch = (e) => {
@@ -84,7 +88,7 @@ const CSSelect = ({handleStorageChange, memberId}) => {
         } catch (error) {
             console.error('Error fetching inquiries:', error);
         }
-    }, [page, searchParams]);
+    }, [page, searchParams, refreshKey]);
 
     useEffect(() => {
         fetchInquiries();
@@ -97,125 +101,113 @@ const CSSelect = ({handleStorageChange, memberId}) => {
 
         for (let i = startPage; i <= endPage; i++) {
             pageNumbers.push(
-                <li key={i}>
-                    <button onClick={() => handlePageChange(i)}>
-                        {i}
-                    </button>
-                </li>
+                <Pagination.Item key={i} active={i === page} onClick={() => handlePageChange(i)}>
+                {i}
+                </Pagination.Item>
             );
         }
 
         return pageNumbers;
     };
 
-    // const memberId = sessionStorage.getItem("member_id");
-
     return (
-        <div>
+        <>
             {/* 헤더 부분 */}
             <MemberHeader handleStorageChange={handleStorageChange} memberId={memberId}/>
 
-            <div>
-                <h1>samsamzo 고객 문의</h1>
-            </div>
-            <section>
-                <div>
-                    <div>
-                        <div>
-                            <div>
-                                <h2>문의 목록</h2>
-                            </div>
-                            <div>
-                                <form onSubmit={handleSearch}>
-                                    <div>
-                                        <label htmlFor="searchContent">검색 기준</label>
-                                        <select
-                                            id="searchContent"
-                                            value={searchContent}
-                                            onChange={handleSearchContentChange}
-                                        >
-                                            <option value="title">제목</option>
-                                            <option value="content">내용</option>
-                                        </select>
-                                        <input
-                                            type="text"
-                                            id="searchKeyword"
-                                            placeholder="검색어를 입력하세요"
-                                            value={searchKeyword}
-                                            onChange={handleSearchKeywordChange}
-                                        />
-                                    </div>
-                                    <button type="submit">검색</button>
-                                </form>
+            <Container style={{ marginTop: '20px' }}>
 
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th>문의 번호</th>
-                                            <th>문의 제목</th>
-                                            <th>문의 일자</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {inquiries.length > 0 ? (
-                                            inquiries.map((item) => (
-                                                <tr key={item.csNumber}>
-                                                    <td>{item.csNumber}</td>
-                                                    <td>{item.csTitle}</td>
-                                                    <td>{item.csDate ? item.csDate.substring(0, 10) : ''}</td>
-                                                    <td>
-                                                        <Link to={`/cs/${item.csNumber}`}>
-                                                            문의 상세 보기
-                                                        </Link>
-                                                    </td>
-                                                </tr>
-                                            ))
-                                        ) : (
-                                            <tr>
-                                                <td colSpan="4">검색 결과가 없습니다.</td>
-                                            </tr>
-                                        )}
-                                    </tbody>
-                                </table>
+                <section>
 
-                                <div>
-                                    <ul>
-                                        <li>
-                                            <button
-                                                onClick={() => handlePageChange(page - 1)}
-                                                disabled={page <= 1}
-                                            >
-                                                이전
-                                            </button>
-                                        </li>
-                                        {renderPageNumbers()}
-                                        <li>
-                                            <button
-                                                onClick={() => handlePageChange(page + 1)}
-                                                disabled={page >= totalPages}
-                                            >
-                                                다음
-                                            </button>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    {/* 로그인 되어있는 상태에서만 문의등록 버튼이 보인다 */}
-                                    {memberId && <Link to="/cs/insert">문의 등록</Link>}
-                                    {searched && (
-                                        <Link to="/" onClick={handleReset}>
-                                            문의 목록
-                                        </Link>
-                                    )}
-                                    <ToMainPageButton />
-                                </div>
-                            </div>
+                    <Row className="align-items-center">
+                        <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                            {/* <Form onSubmit={handleSearch} className="d-flex"> */}
+                                <Dropdown onSelect={handleSearchContentChange} className="mr-2">
+                                    <Dropdown.Toggle className='custom-dropdown' variant="secondary" id="dropdown-basic">
+                                    {searchContent === 'title' ? '제목' : '내용'}
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu className='custom-dropdown-menu'>
+                                    <Dropdown.Item eventKey="title">
+                                        제목
+                                    </Dropdown.Item>
+                                    <Dropdown.Item eventKey="content">
+                                        내용
+                                    </Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            <Form onSubmit={handleSearch} className="d-flex">
+                                <Form.Control
+                                className="search-box mr-2"
+                                type="text"
+                                id="searchKeyword"
+                                placeholder="검색어를 입력하세요"
+                                value={searchKeyword}
+                                onChange={handleSearchKeywordChange}
+                                />
+                                <Button className='search-btn' type="submit">검색</Button>
+                            </Form>
                         </div>
+                    </Row>
+
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <h2 className="register-text">문의 목록</h2>
+                        {/* {memberId && <Link to="/cs/insert">문의 등록</Link>} */}
+
+                        {memberId &&
+                            <div className='cs-btn'>
+                                <Link to="/cs/insert" className="btn btn-primary">문의 등록</Link>
+                            </div>
+                        }
+
+                        
                     </div>
-                </div>
-            </section>
-        </div>
+
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>문의 번호</th>
+                                <th>문의 제목</th>
+                                <th>문의 일자</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {inquiries.length > 0 ? (
+                                inquiries.map((item) => (
+                                <tr key={item.csNumber}>
+                                    <td>{item.csNumber}</td>
+                                    <td>{item.csTitle}</td>
+                                    <td>{item.csDate ? item.csDate.substring(0, 10) : ''}</td>
+                                    <td>
+                                        <Link to={`/cs/${item.csNumber}`} className="btn btn-info">
+                                            문의 상세 보기
+                                        </Link>
+                                    </td>
+                                </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                <td colSpan="4">검색 결과가 없습니다.</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </Table >
+
+                    <Pagination className="pagination-controls">
+                        <Pagination.Prev onClick={() => handlePageChange(page - 1)} disabled={page <= 1} />
+                        {renderPageNumbers()}
+                        <Pagination.Next onClick={() => handlePageChange(page + 1)} disabled={page >= totalPages} />
+                    </Pagination>
+
+                    <div className="pagination-actions">
+                        {searched && (
+                            <Button className='cs-select-btn' variant="secondary" onClick={handleReset}>전체 목록 보기</Button>
+                        )}
+                    </div>
+
+                </section>
+            </Container>
+        </>
     );
 };
 
